usrivastava-nvidia/frr

commit 784cf95c4377ec84b25fb5801fdfaa20450325de

bgpd: Try to handle software version capability with the new encoding format
v15 of https://datatracker.ietf.org/doc/html/draft-abraitis-bgp-version-capability
defined a new encoding for this capability by eliminating an extra field for
capability length inside the capability's value. Instead, use just the capability
length value.

When a BGP node sends a new encoding, FRR can't parse it and drops the session.

Let's try to be more polite and do not give up, check if capability header
length is the same as the capability's length inside the value.

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_packet.c
@@ -3672,16 +3672,30 @@ static void bgp_dynamic_capability_software_version(uint8_t *pnt, int action,
 {
 	uint8_t *data = pnt + 3;
 	uint8_t *end = data + hdr->length;
-	uint8_t len = *data;
+	uint8_t len = hdr->length;
+	uint8_t cap_value_len_field = *data + 1;
 	char soft_version[BGP_MAX_SOFT_VERSION + 1] = {};
+	bool old_encoding = false;
 
 	if (action == CAPABILITY_ACTION_SET) {
+		/* For backward compatibility.
+		 * Older draft versions defined the length field inside
+		 * the capability's value. Newer versions use just the
+		 * capability's length which is hdr->length.
+		 */
+		if (cap_value_len_field == len) {
+			len = *data;
+			old_encoding = true;
+		}
+
 		if (data + len + 1 > end) {
 			zlog_err("%pBP: Received invalid Software Version capability length %d",
 				 peer, len);
 			return;
 		}
-		data++;
+
+		if (old_encoding)
+			data++;
 
 		if (len > BGP_MAX_SOFT_VERSION)
 			len = BGP_MAX_SOFT_VERSION;

bgpd/bgp_open.c
@@ -971,11 +971,22 @@ static int bgp_capability_software_version(struct peer *peer, struct peer_connec
 					   struct capability_header *hdr)
 {
 	struct stream *s = BGP_INPUT(connection);
+	struct stream *dup = stream_dup(s);
 	char str[BGP_MAX_SOFT_VERSION + 1];
 	size_t end = stream_get_getp(s) + hdr->length;
-	uint8_t len;
+	uint8_t len = hdr->length;
+	uint8_t cap_value_len_field = stream_getc(dup) + 1;
 
-	len = stream_getc(s);
+	/* For backward compatibility.
+	 * Older draft versions defined the length field inside
+	 * the capability's value. Newer versions use just the capability's
+	 * length which is hdr->length.
+	 */
+	if (cap_value_len_field == len)
+		len = stream_getc(s);
+
+	stream_free(dup);
+
 	if (stream_get_getp(s) + len > end) {
 		flog_warn(
 			EC_BGP_CAPABILITY_INVALID_DATA,

commit b5280177cbf14240172a14e88bb120667284fa72

bgpd: Prevent out-of-bound reading handling soft version dynamic capability
Fixes: 784cf95c4377ec84b25fb5801fdfaa20450325de ("bgpd: Try to handle software version capability with the new encoding format")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_packet.c
@@ -3742,33 +3742,22 @@ static void bgp_dynamic_capability_software_version(uint8_t *pnt, int action,
 	uint8_t *data = pnt + 3;
 	uint8_t *end = data + hdr->length;
 	uint8_t len = hdr->length;
-	uint8_t cap_value_len_field = *data + 1;
+	uint8_t cap_value_len_field;
 	char soft_version[BGP_MAX_SOFT_VERSION + 1] = {};
 
 	if (action == CAPABILITY_ACTION_SET) {
+		if (data >= end)
+			return;
+
 		/* For backward compatibility.
 		 * Older draft versions defined the length field inside
 		 * the capability's value. Newer versions use just the
 		 * capability's length which is hdr->length.
 		 */
+		cap_value_len_field = *data + 1;
 		if (cap_value_len_field == len) {
 			len = *data;
-
-			if (data + len + 1 > end) {
-				flog_err(EC_BGP_CAPABILITY_INVALID_LENGTH,
-					 "%pBP: Received invalid Software Version capability length %d",
-					 peer, len);
-				return;
-			}
-
 			data++;
-		} else {
-			if (data + len > end) {
-				flog_err(EC_BGP_CAPABILITY_INVALID_LENGTH,
-					 "%pBP: Received invalid Software Version capability length %d",
-					 peer, len);
-				return;
-			}
 		}
 
 		if (len > BGP_MAX_SOFT_VERSION)